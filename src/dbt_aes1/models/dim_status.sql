245f82b0ce57431992016501b6a545b8798027f8
Sergio Ribeiro <sergiorcerqueira@gmail.com>
2025-03-18 12:00:00 -0300

    feat: Criação dos modelos dbt (#4)

---
+++ src/dbt_aes1/models/dim_status.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+WITH status_unicos AS (
+    SELECT DISTINCT status AS cd_status
+    FROM {{ source('datalake_silver', 'aes1_transacoes') }}
+)
+
+SELECT
+    md5(cd_status) AS id_status_sk,
+    cd_status,
+    CASE
+        WHEN cd_status = 'pending' THEN 'Pendente'
+        WHEN cd_status = 'approved' THEN 'Aprovada'
+        WHEN cd_status = 'denied' THEN 'Recusada'
+        ELSE cd_status
+    END AS ds_status,
+    
+    -- Estrutura SCD Tipo 2
+    CAST('2020-01-01 00:00:00' AS TIMESTAMP) AS start_date,
+    CAST(NULL AS TIMESTAMP) AS end_date,
+    TRUE AS is_current
+FROM status_unicos
